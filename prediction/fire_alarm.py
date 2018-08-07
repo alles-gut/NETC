7d54c37d9979a94839e85c42ea40867f7ad3f75d
alles-gut <jhu4142@gmail.com>
2018-08-07 16:28:55 +0900

    Predict fire

---
+++ prediction/fire_alarm.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split
+import os
+
+#reading data
+csv = pd.read_csv("data.csv")
+
+#choose data
+csv_data = csv[["temperature", "humidity"]]
+csv_label = csv["label"]
+
+#split data into train and test
+data_train, data_test, label_train, label_test = \
+    train_test_split(csv_data, csv_label)
+
+#training data
+clf = svm.LinearSVC()
+clf.fit(data_train, label_train)
+
+#predict data
+predict = clf.predict(data_test)
+
+#test out
+ac_score = metrics.accuracy_score(label_test, predict)
+# cl_report + metrics.classification_report(label_test, prediction)
+print("Model occuracy =",ac_score)
+
+#get test_set.csv file from influxdb
+os.system('influx -database tstest -format csv -execute \'select * from table03\' > test_set.csv')
+print("Finish querying")
+
+test_set = pd.read_csv("test_set.csv")
+
+for line in range(0,len(test_set)):
+
+    print("line num:",line)
+
+    a=test_set['temperature'][line]
+    b=test_set['humidity'][line]
+
+    print("recap",a,b)
+    #print(clf.predict([[int(a) ,int(b)]]))
+    s=clf.predict([[int(a) ,int(b)]])
+    print(s)
+    # print("report =\n", cl_report)
+
